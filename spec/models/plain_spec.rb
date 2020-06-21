965aa6ca0852b05bf8597bb3346bb538d003b375
Taras Bilous <tbilous@gmail.com>
2020-06-21 12:20:37 +0300

    models specs

--- spec/models/plain_spec.rb
+++ spec/models/plain_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Plain, type: :model do
   it { is_expected.to have_many(:plain_histories).with_dependent(:destroy) }
+  it { is_expected.to validate_inclusion_of(:state).to_allow(Options::Plain::States.all) }
+
+  describe '#write_history' do
+    subject { described_class.create }
+
+    it { expect { subject }.to change(PlainHistory, :count).by(1) }
+  end
 end
