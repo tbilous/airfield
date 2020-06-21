a1361e266e15948d798af59b8bb9013ec019ae8d
Taras Bilous <tbilous@gmail.com>
2020-06-21 12:09:47 +0300

    add plain subscription

--- app/channels/plain_channel.rb
+++ app/channels/plain_channel.rb
@@ -1,5 +1,11 @@
 class PlainChannel < ApplicationCable::Channel
-  def subscribed(id)
-    stream_from "plain_#{id}"
+  def subscribed
+    reject if params['plain_id'].blank?
+    plain = Plain.find_by(id: params['plain_id'])
+    reject unless plain
+
+    stream_from "plain_#{plain&.id}"
+  rescue Mongoid::Errors::DocumentNotFound
+    reject
   end
 end
--- spec/channels/plain_channel_spec.rb
+++ spec/channels/plain_channel_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PlainChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:plain) { Plain.create }
+
+  context 'params is wring' do
+    it 'rejects when no id provided' do
+      subscribe
+      expect(subscription).to be_rejected
+    end
+  end
+
+  context 'params is right' do
+    before { subscribe(plain_id: plain.id.to_s) }
+
+    it 'subscribes to plain stream when id provided' do
+      expect(subscription).to be_confirmed
+    end
+
+    it 'plain stream when id provided' do
+      expect(subscription).to have_stream_from("plain_#{plain.id}")
+    end
+  end
 end
